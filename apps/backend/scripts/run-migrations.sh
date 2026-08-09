fe6b255ec71b12648fea298aa5b87e7c55ff087c
Sergei Rebrin <s.rebrin@g.nsu.ru>
2026-08-09 12:48:32 +0700

    fix(generator): updated run-migrations

--- apps/backend/scripts/run-migrations.sh
+++ apps/backend/scripts/run-migrations.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 set -eu
 
-mkdir -p /tmp/migrations
-for f in /migrations/*.sql; do
-  base=$(basename "$f")
-  cp "$f" "/tmp/migrations/${base%.sql}.up.sql"
-done
-
-migrate -path=/tmp/migrations -database="postgres://${POSTGRES_USER:-avito-recap}:${POSTGRES_PASSWORD:-avito-recap}@postgres:5432/${POSTGRES_DB:-avito-recap}?sslmode=disable" up
+migrate -path=/migrations -database="postgres://${POSTGRES_USER:-avito-recap}:${POSTGRES_PASSWORD:-avito-recap}@postgres:5432/${POSTGRES_DB:-avito-recap}?sslmode=disable" "${@:-up}"
